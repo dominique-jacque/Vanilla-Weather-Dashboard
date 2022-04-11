5448d18474a78947c1c8479b17d09a19a121dfd9
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-11 14:46:45 -0400

    Added displayWeather function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 function displayTemperature(response) {
-    console.log(response);
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#current-temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    
+
 }
 let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
