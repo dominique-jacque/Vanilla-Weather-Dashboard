69d41740d7d029de745fe5d1921953fe79e40f1e
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-11 14:29:50 -0400

    Added Api Key & Url

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function displayTemperature(response) {
+    console.log(response);
+}
+let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
