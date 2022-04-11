2ec97b0e47761d8a46e6564fbb8f22a2586c6e65
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-11 15:07:16 -0400

    Added more elements to displayTemperature function

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
     let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
     
 
 }
