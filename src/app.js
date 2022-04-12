1f9c6284d9c21d6c2f4b53d88bc649c35b59ef66
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-11 20:00:44 -0400

    Added Search Form

--- src/app.js
+++ src/app.js
@@ -34,8 +34,20 @@ function displayTemperature(response) {
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Paris";
-let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+
+    let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
