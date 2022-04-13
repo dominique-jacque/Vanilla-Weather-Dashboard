72016dbe299c304bf827a157104302501ba678f4
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-13 14:11:14 -0400

    Following Lesson, added displayForecast function in JS

--- src/app.js
+++ src/app.js
@@ -13,6 +13,33 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast () {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class = "row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) {
+    forecastHTML = 
+        forecastHTML + 
+    `
+    <div class = "col-2">
+        <div class = "weather-forecast-date">${days}</div>
+            <img
+                src = "http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="42" />
+            <div class="weather-forecast-temperatures">
+                <span class = "weather-forecast-temperature-max">
+                18</span>
+                <span class = "weather-forecast-temperature-min">
+                12</span>
+        </div> 
+    </div>`
+    });
+forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 
 function displayTemperature(response) {
     celsiusTemp = response.data.main.temp;
@@ -76,4 +103,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemp)
+celsiusLink.addEventListener("click", displayCelsiusTemp);
