c8e78c7db7a17cab377d3917f75c70b469bb3272
dominique-jacque <dominiquejacqueline@yahoo.com>
2022-04-19 17:05:52 -0400

    Added Current Location button

--- src/app.js
+++ src/app.js
@@ -49,10 +49,30 @@ function displayForecast(response) {
         }
     });
 forecastHTML = forecastHTML + `</div>`;
+
     forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates){
+
+
+function getCurrentPosition (event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+
+let currentCityButton = document.querySelector("#current-city");
+currentCityButton.addEventListener("click", getCurrentPosition);
+
+function getLocation(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -107,6 +127,7 @@ function displayFahrenheitTemp(event) {
     let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
+
  function displayCelsiusTemp(event) {
      event.preventDefault();
      celsiusLink.classList.add("active");
