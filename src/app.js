bb818ea98e9efc15697a18c625b0610dc3a6fb61
Dominique <dominiquejacqueline@yahoo.com>
2025-01-12 22:08:19 -0500

    Updated API key and Reorganized project structure

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ forecastHTML = forecastHTML + `</div>`;
 function getLocation(position) {
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
-    let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+    let apiKey = "204a65211e3e883c18f75fbf3bc7c7c6";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
@@ -72,7 +72,7 @@ currentCityButton.addEventListener("click", getCurrentPosition);
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+    let apiKey = "204a65211e3e883c18f75fbf3bc7c7c6";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
@@ -105,7 +105,7 @@ function displayTemperature(response) {
 
 function search(city) {
 
-    let apiKey = "08e97ecdda5ea0cf3c620c610617c3ee";
+    let apiKey = "204a65211e3e883c18f75fbf3bc7c7c6";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
